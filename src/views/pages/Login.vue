79c990899d067e78376c4520cac7b87d0bcb995e
Orazbaev Ayan <orazbaev.ayan@gmail.com>
2021-08-07 22:14:09 +0600

    added reset.css

---
+++ src/views/pages/Login.vue
@@ -0,0 +1,21 @@
+<template>
+	<form>
+		<label for="iin">ИИН</label>
+		<input id="iin" type="text">
+		<label for="password">Пароль</label>
+		<input id="password" type="password">
+	</form>
+</template>
+
+<script>
+	
+</script>
+
+<style lang="scss">
+	form {
+		max-width: 100%;
+		width: 300px;
+		display: flex;
+		flex-direction: column;
+	}
+</style>
