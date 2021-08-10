67419ec555a34f0d8e2847e554ed566b05c7c311
Orazbaev Ayan <orazbaev.ayan@gmail.com>
2021-08-11 03:49:10 +0600

    added 404 page

--- src/router/index.js
+++ src/router/index.js
@@ -17,6 +17,11 @@ const routes = [
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "about" */ '../views/pages/Login.vue')
+  },
+  {
+    path: '/:pathMatch(.*)*',
+    name: '404',
+    component: () => import('../views/pages/404.vue')
   }
 ]
 
---
+++ src/views/pages/404.vue
@@ -0,0 +1,25 @@
+<template>
+	<div id="content">
+		<h1>404</h1>
+		<h1>Страница не найдена</h1>
+		<a href="/">На главную</a>
+	</div>
+</template>
+
+<script>
+	export default {
+	}
+</script>
+
+<style lang="scss" scoped="">
+#content {
+	display: flex;
+	flex-direction: column;
+	flex-grow: 1;
+	justify-content: center;
+	align-items: center;
+	h1 {
+		margin-bottom: 16px;
+	}
+}
+</style>
