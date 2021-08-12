f42f375569a3f2a84b331677276d21b87c906aa3
Orazbaev Ayan <orazbaev.ayan@gmail.com>
2021-08-12 23:39:43 +0600

    Home page changes

--- src/views/pages/Home.vue
+++ src/views/pages/Home.vue
@@ -1,20 +1,19 @@
 <template>
   <div id="header">
     <h1>Home</h1>
+    <SelectLanguage />
+    <span>{{ user.name }}</span>
     <LogoutButton />
   </div>
   <div id="content">
-    {{ user }}
-    <br>
-    <br>
-    <SelectLanguage />
+
   </div>
 </template>
 
 <script>
 import LogoutButton from '@/components/LogoutButton'
 import SelectLanguage from '@/components/SelectLanguage'
-import { mapGetters, mapActions } from 'vuex';
+import { mapGetters } from 'vuex';
 
 export default {
   name: 'Home',
@@ -27,11 +26,6 @@ export default {
       user: 'auth/user'
     })
   },
-  methods: {
-    ...mapActions({
-      login: 'auth/login'
-    })
-  }
 }
 </script>
 
