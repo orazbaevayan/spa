fc3861961cb1a9c1cb2a96e4dc8904e860897280
Orazbaev Ayan <orazbaev.ayan@gmail.com>
2021-08-20 00:27:28 +0600

    added left sidebar

--- src/components/Content.vue
+++ src/components/Content.vue
@@ -3,20 +3,36 @@
 		<div id="content">
 			<slot />
 		</div>
+		<LeftSidebar />
 	</div>
 </template>
+
+<script>
+	import LeftSidebar from '@/components/LeftSidebar'
+
+	export default {
+		components: {
+			LeftSidebar
+		}
+	}
+</script>
 
 <style lang="scss" scoped>
 #body {
 	max-width: 100%;
-	width: 100%;
+	width: 1140px;
 	display: flex;
-	justify-content: center;
+	flex-direction: column;
+	flex-grow: 1;
+	position: relative;
 	#content {
+		display: flex;
+		flex-direction: column;
+		flex-grow: 1;
 		max-width: 100%;
 		width: 840px;
 		padding-left: 300px;
-		margin-top: 41px;
+		padding-top: 41px;
 	}
 }
 </style>
---
+++ src/components/LeftSidebar.vue
@@ -0,0 +1,23 @@
+<template>
+	<div id="left-sidebar">
+		<router-link class="link" :to="{ name: 'Home' }">{{ $t('pages.Главная') }}</router-link>
+		<router-link class="link" :to="{ name: 'Courses' }">{{ $t('pages.Курсы') }}</router-link>
+	</div>
+</template>
+
+<style lang="scss" scoped>
+#left-sidebar {
+	position: fixed;
+	max-width: 80%;
+	width: 300px;
+	top: 0;
+	bottom: 0;
+	padding-top: 41px;
+	.link {
+		display: block;
+		text-decoration: none;
+		padding: 8px;
+		line-height: 100%;
+	}
+}
+</style>
