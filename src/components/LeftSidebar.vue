2aa8d5a7b2ec09334e5f48b25bdace5dffb1224b
Orazbaev Ayan <orazbaev.ayan@gmail.com>
2021-08-22 22:31:29 +0600

    save

--- src/components/LeftSidebar.vue
+++ src/components/LeftSidebar.vue
@@ -2,12 +2,6 @@
 	<div id="left-sidebar" :class="{ open: open, moves: moves }" @touchstart="touchstart" @touchend="touchend" @touchmove="touchmove">
 		<router-link class="link" :to="{ name: 'Home' }">{{ $t('pages.Главная') }}</router-link>
 		<router-link class="link" :to="{ name: 'Courses' }">{{ $t('pages.Курсы') }}</router-link>
-		<br>
-		{{ 'diff: ' + diff }}
-		<br>
-		{{ 'lastTouch: ' + lastTouch }}
-		<br>
-		{{ 'speed: ' + speed }}
 	</div>
 </template>
 
@@ -19,10 +13,12 @@
 		data() {
 			return {
 				moves: false,
-				lastTouch: null,
-				diff: 0,
-				lastPosX: 0,
-				lastTime: 0,
+				lastTouch: {
+					identifier: null,
+					clientX: 0,
+					timestamp: 0
+				},
+				diffX: 0,
 				speed: 0
 			}
 		},
@@ -32,8 +28,7 @@
 			}),
 			setMoves(event) {
 				event.targetTouches.length ? this.moves = true : this.moves = false;
-				this.activeTouch(event, this.lastTouch) ? this.diff = event.target.offsetLeft - this.activeTouch(event, this.lastTouch).clientX : this.diff = 0;
-				this.touches = event.touches.length;
+				this.activeTouch(event, this.lastTouch.identifier) ? this.diffX = event.target.offsetLeft - this.activeTouch(event, this.lastTouch.identifier).clientX : this.diffX = 0;
 			},
 			activeTouch(event, identifier) {
 				for (var i = 0, len = event.targetTouches.length; i < len; i++) {
@@ -44,34 +39,31 @@
 				}
 			},
 			touchstart(event) {
-				this.lastTouch = event.changedTouches.item(0).identifier;
+				this.lastTouch.identifier = event.changedTouches.item(0).identifier;
 				this.setMoves(event);
 			},
 			touchmove(event) {
-				let left = this.activeTouch(event, this.lastTouch).clientX + this.diff;
-				if (event.changedTouches[0].identifier === this.lastTouch) {
-					this.speed = (event.changedTouches[0].clientX - this.lastPosX)/(event.timeStamp - this.lastTime);
-					this.lastTime = event.timeStamp;
-					this.lastPosX = event.changedTouches[0].clientX;
+				let left = this.activeTouch(event, this.lastTouch.identifier).clientX + this.diffX;
+				if (event.changedTouches[0].identifier === this.lastTouch.identifier) {
+					this.speed = (event.changedTouches[0].clientX - this.lastTouch.clientX)/(event.timeStamp - this.lastTouch.timeStamp);
+					this.lastTouch.timeStamp = event.timeStamp;
+					this.lastTouch.clientX = event.changedTouches[0].clientX;
 				}
 				if (left > 0) {
 					event.target.style.left = '0px';
 				} else if (left < -event.target.offsetWidth) {
 					event.target.style.left = -event.target.offsetWidth + 'px';
 				} else {
-					event.target.style.left = this.activeTouch(event, this.lastTouch).clientX + this.diff + 'px';
+					event.target.style.left = this.activeTouch(event, this.lastTouch.identifier).clientX + this.diffX + 'px';
 				}
 			},
 			touchend(event) {
 				this.setMoves(event);
 				let left = event.target.offsetLeft;
-				if (left < -(event.target.offsetWidth / 2)) {
+				if (left < -(event.target.offsetWidth / 2) || this.speed < -0.1) {
 					this.SET_FOCUS();
 				} else {
 					this.SET_FOCUS(this.$options.name);
-				}
-				if (this.speed < -0.1) {
-					this.SET_FOCUS();
 				}
 				if (!event.targetTouches.length) {
 					event.target.style.left = '';
