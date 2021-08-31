25145e7ba8e4bd2322c76ffba84f72ffb0f07282
Orazbaev Ayan <orazbaev.ayan@gmail.com>
2021-08-31 23:01:26 +0600

    while sidebars open prevent touch-action on body

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,31 @@
 <template>
   <component :is="$route.meta.layout">
-    <router-view/>
+    <router-view />
   </component>
   <Overlay />
 </template>
 
 <script>
   import Overlay from '@/components/Overlay'
+  import { mapGetters } from 'vuex'
 
   export default {
     components: {
       Overlay
-    }
+    },
+    computed: {
+      ...mapGetters({
+        'focus': 'ui/focus'
+      })
+    },
+    watch: {
+      focus(newFocus) {
+        let noScrollFocusElements = [
+          'left-sidebar',
+          'right-sidebar'
+        ];
+        document.body.style['touch-action'] = noScrollFocusElements.includes(newFocus) ? 'none' : '';
+      }
+    },
   }
 </script>
