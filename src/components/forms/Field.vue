0bb87ce3da8a59c00fec319094b10c503ca6f101
Orazbaev Ayan <orazbaev.ayan@gmail.com>
2021-10-07 11:17:28 +0600

    before option created

--- src/components/forms/Field.vue
+++ src/components/forms/Field.vue
@@ -13,17 +13,9 @@
 		<div class="col-12 p-2">
 			<label class="form-label" for="type">{{ $t(`models.ALL['Тип']`) }}</label>
 			<select class="form-select form-select-sm" name="type" id="type" :value="value.type" :readonly="!canEdit">
-				<option></option>
-				<option value="1">1</option>
-				<option value="1">2</option>
-			</select>
-		</div>
-		<div class="col-12 p-2">
-			<label class="form-label" for="category">{{ $t(`models.ALL['Категория']`) }}</label>
-			<select class="form-select form-select-sm" name="category" id="category" :value="value.category" :readonly="!canEdit">
-				<option></option>
-				<option value="student">Студент</option>
-				<option value="group">Группа</option>
+				<option value="Text">Текст</option>
+				<option value="Select">Выбор</option>
+				<option value="Date">Дата</option>
 			</select>
 		</div>
 		<div class="w-100 text-center">
